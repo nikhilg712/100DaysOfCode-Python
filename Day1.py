0174278276a63cbfc32e1c787336cb89b32eb8bc
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-02 19:41:22 +0530

    first day initial commit

---
+++ Day1.py
@@ -0,0 +1,18 @@
+# print("Hello "+ input("What's your name?\n"))
+
+# number1 = int(input("enter number one\n"))
+# number2 = int(input("enter number two\n"))
+# print(int(number1/number2))
+
+# name = input()
+# num = int(len(name))
+# print(num)
+
+a= input()
+b= input()
+
+temp=a
+a=b
+b=temp
+
+print(a +" "+ b)
