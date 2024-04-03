629b1123ad9445c27109679e6b50edfded95dd35
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-03 19:37:41 +0530

    Day 2 project- Tip calculator

---
+++ Day2.py
@@ -0,0 +1,10 @@
+# a = str(123)
+# print(type(a))
+
+# Day 2 Project: tip Calculator
+
+print("Welcome to the Tip Calculator")
+totalBill = int(input("What was the Total Bill?\n₹"))
+Tip = int(input("What is the tip amount you want to give?\n₹"))
+People = int(input("What is the total number of people?\n"))
+print("Each person has to pay: ₹"+str(int((totalBill-Tip)/People)))
