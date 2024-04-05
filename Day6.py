5b6423ee58d4f666eadc935afcddd7a8881d2c51
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-05 21:36:07 +0530

    Day 6 Project - Escaping the Maze

---
+++ Day6.py
@@ -0,0 +1,17 @@
+# Day 6 concept learnt - Functions
+
+# Day 6 Project- Escaping the Maze
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
