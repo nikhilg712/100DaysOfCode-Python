004dcd99ef9a480e70023bedee17daba4a599e54
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-07 00:46:15 +0530

    Day 8 Project - Cipher Decipher

--- Day8.py
+++ Day8.py
@@ -8,4 +8,48 @@
 
 # Day 8 Project - Caesar Cipher
 
+def encrypt():
+    word = input("Enter the word you want to cipher\n").lower()
+    shift = int(input("enter the amount of shift\n"))
+    initialLetterList = []
+
+    for letter in word:
+        initialLetterList.append(ord(letter))
+
+    for i in range(len(initialLetterList)):
+        initialLetterList[i] += shift
+
+    wordArray = []
+    for ascii in initialLetterList:
+        wordArray.append(chr(ascii))
+
+    word = "".join(wordArray)    
+    print("the ciphered word is:",word)
+
+def decrypt():
+    word = input("Enter the word you want to cipher\n").lower()
+    shift = int(input("enter the amount of shift\n"))
+    initialLetterList = []
+
+    for letter in word:
+        initialLetterList.append(ord(letter))
+
+    for i in range(len(initialLetterList)):
+        initialLetterList[i] -= shift
+
+    wordArray = []
+    for ascii in initialLetterList:
+        wordArray.append(chr(ascii))
+
+    word = "".join(wordArray)    
+    print("the ciphered word is:",word)
+
+
+
+wish = input("Type 'encode' to encrypt or 'decode' to decrypt:\n")
+
+if wish == "encode":
+    encrypt()
+else:
+    decrypt()    
 
