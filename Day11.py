e2b30e3f7175cc6267024eedc67e3e2797653db9
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-09 14:05:14 +0530

    Day 11 Capstone Project - BlackJack Game

--- Day11.py
+++ Day11.py
@@ -7,24 +7,46 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 firstCards = [cards[random.randint(0,12)],cards[random.randint(0,12)]]
 computerCards = [cards[random.randint(0,12)],cards[random.randint(0,12)]]
 
+total =0
+
 def myCardSum(cardList):
-    sum =0
+    global total
     for card in cardList:
-        sum=sum+card
+        total=total+card
+    return total    
 
 def firstHit():
     print(f"Your Cards are: {firstCards}. Current Score: {myCardSum(firstCards)}")
-    print(f"computer's first card{computerCards[0]}")
+    print(f"computer's first card: {computerCards[0]}")
+    hitOrPass()
+
+def nextHits():
+    # global total
+    print(f"Your Cards are: {firstCards}. Current Score: {total}")
+    if total==21:
+        print("You won")
+        return
+    if total>21:
+        print("Your sum greater than 21. You lost")
+        return
+    
+
 
 def hit():
     wish = input("Do you want to play a game of BlackJack? Type 'Y'or 'N'")
     if wish == 'Y':
         firstHit()
 
-hitOrPass = input("Type 'Y' to get another card or 'N' to Pass")
-if hitOrPass == 'Y':
-    firstCards.append(cards[random.randint(0,12)])
-    firstHit()
+def hitOrPass():
+    global total
+    hitPass =input("Type 'Y' to get another card or 'N' to Pass")
+    if hitPass == 'Y':
+        firstCards.append(cards[random.randint(0,12)])
+        total+=firstCards[len(firstCards)-1]
+        nextHits()
+    else:
+        print(f"computer's second card: {computerCards[1]}")
+
 
 firstHit()
 
